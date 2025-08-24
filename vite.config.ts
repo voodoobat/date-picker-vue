38ad7f1c3ec78b600c6a555d123353b92bf433a8
Vova Rodin <mynameisvova@yahoo.com>
2025-08-24 09:23:35 +0300

    chore: adds postcss plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import autoImport from 'unplugin-auto-import/vite'
+import autoprefixer from 'autoprefixer'
+import nesting from 'postcss-nesting'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueRouter from 'unplugin-vue-router/vite'
@@ -15,6 +17,11 @@ export default defineConfig({
     vueDevTools(),
     vueRouter({}),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer(), nesting()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
